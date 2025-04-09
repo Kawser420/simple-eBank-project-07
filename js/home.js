84c7b12c61d18c6d26d021d3f4d1f686ee3ea20b
Kawser420 <muhammadkawser202@gmail.com>
2025-04-10 03:24:46 +0600

    home javasript input in complete

--- js/home.js
+++ js/home.js
@@ -6,4 +6,19 @@ document
     const inputAmount = document.getElementById("input-amount").value;
     const inputPin = document.getElementById("input-pin").value;
     console.log(inputAmount, inputPin);
+    // check pin
+    if (inputPin === "1234") {
+      const inputAmount = document.getElementById("input-amount").value;
+      const mainBalance = document.getElementById("main-balance").innerText;
+      console.log(inputAmount, mainBalance);
+      //   parseFloat
+      const inputAmountField = parseFloat(inputAmount);
+      const mainBalanceUpdated = parseFloat(mainBalance);
+      const newBalance = inputAmountField + mainBalanceUpdated;
+      console.log(newBalance);
+      //   final step
+      document.getElementById("main-balance").innerText = newBalance;
+    } else {
+      alert("Failed Pin Number. please try again!");
+    }
   });
