220555649be3bf398676f9568cd84033d2103eca
Kawser420 <muhammadkawser202@gmail.com>
2025-04-10 00:25:39 +0600

    home js added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log("home clicked");
+// add money
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const inputAmount = document.getElementById("input-amount").value;
+    const inputPin = document.getElementById("input-pin").value;
+    console.log(inputAmount, inputPin);
+  });
