ed4bd5ed0638a6846f4f2bc86377db723cbdc149
Kawser420 <muhammadkawser202@gmail.com>
2025-04-21 23:31:53 +0600

    site is finishing way

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,53 +1,9 @@
-// add money start
-// document
-//   .getElementById("btn-add-money")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     const inputAmount = document.getElementById("input-amount").value;
-//     const inputPin = document.getElementById("input-pin").value;
-//     // console.log(inputAmount, inputPin);
-//     // check pin
-//     if (inputPin === "1234") {
-//       const inputAmount = document.getElementById("input-amount").value;
-//       const mainBalance = document.getElementById("main-balance").innerText;
-//       // console.log(inputAmount, mainBalance);
-//       //   parseFloat
-//       const inputAmountField = parseFloat(inputAmount);
-//       const mainBalanceUpdated = parseFloat(mainBalance);
-//       const newBalance = inputAmountField + mainBalanceUpdated;
-//       // console.log(newBalance);
-//       //   final step
-//       document.getElementById("main-balance").innerText = newBalance;
-//     } else {
-//       alert("Failed Pin Number. please try again!");
-//     }
-//   });
-// add money end
-
-// add money start
+// add money
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const addMoney = getInputFieldValueById();
-    console.log("add money", addMoney);
+    const addMoney = document.getElementById("input-add-money").value;
+    const addPinNumber = document.getElementById("input-pin-number").value;
+    console.log(addMoney, addPinNumber);
   });
-// cash out start
-document
-  .getElementById("btn-cash-out")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    const cashInputMoney = document.getElementById("cash-input-money").value;
-    const cashInputMoneyNumber = parseFloat(cashInputMoney);
-    const cashInputPin = document.getElementById("cash-input-pin").value;
-    if (cashInputPin === "1234") {
-      const mainBalance = document.getElementById("main-balance").innerText;
-      const mainBalanceNumber = parseFloat(mainBalance);
-      const remainingBalance = mainBalanceNumber - cashInputMoneyNumber;
-      // cash out remaining balance
-      document.getElementById("main-balance").innerText = remainingBalance;
-    } else {
-      alert("Failed cash out. please try again!");
-    }
-  });
-// cash out end
